aa92f0a04afce1ff7300c60d1e33844c6f42d98b
Markus Opolka <markus@martialblog.de>
2020-08-21 12:31:48 +0200

    add x scale

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var data = [
   { id: 0, data: ["elem1", "elem2"]},
   { id: 1, data: ["elem3", "elem4"]},
-  { id: 3, data: ["elem5", "elem6", "elem7"]}
+  { id: 3, data: ["elem5", "elem6", "elem7"]},
+  { i4: 3, data: ["elem5", "elem6", "elem7"]}
 ]
 
 const width = 600;
 const height = 600;
+
+const xScale = d3.scaleBand()
+      .domain(data.map(d => d.id))
+      .range([0, width])
 
 const container = d3.select("#container").append("svg")
     .attr("width", width)
@@ -15,16 +20,17 @@ container.selectAll('rect')
   .data(data)
   .enter().append('g')
   .attr("class", "stack")
+  .attr('transform', function(d, i) {
+    return "translate(" + xScale(d.id) + ", 0)"
+  })
   .each(function(elem, idx){
     d3.select(this).selectAll('rect')
       .data(elem.data)
       .enter().append('rect')
       .attr("class", "elem")
-      .attr('y', function(elem, idx) { return idx * 100; })
-      .attr('x', function() { return idx * 100; })
-      .attr('width', function() { return 90; })
-      .attr('height', function() { return 90; })
+      .attr('y', function(elem, idx) { return idx * 200; })
+      //.attr('x', function() { return idx * 200; })
+      .attr('width', function() { return 100; })
+      .attr('height', function() { return 50; })
       .attr('fill', 'red')
   });
-
-//   .attr('x', d => xScale(d.id))
